672c0a63d2f31859338be301d06e09cddea38c69
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2013-09-25 01:30:20 +0900

    Update JavaHelloWorld.java

--- q431/JavaHelloWorld.java
+++ q431/JavaHelloWorld.java
@@ -5,6 +5,21 @@
 //CodeIQ  Q431
 // https://codeiq.jp/ace/cielavenir/q431
 //---------------------------------------
+//問題文(一部引用)
+// 標準出力に
+// Hello World
+// と出力するプログラムを作成して下さい。
+// 
+// ただし、数値、文字及び文字列リテラルを解答に含めることはできません。
+// Perlのqqやqw、Rubyの%Q、%q、%wなども避けたほうが評価が高くなります。
+// 言語仕様をフル活用して下さい！
+// 
+// プログラミング言語は
+// AppleScript(osascript)/C/C++/C#/Clojure/D/Erlang/Fortran/Go/Groovy/Haskell/
+// Hello Algorithm/HSP/Java/JavaScript(Node.js)/Kuin/Lisp/Lua/OCaml/Pascal/
+// Perl/PHP/Pike/Python/R/Ruby/Scala/Scheme/Smalltalk/VB.Net
+// のいずれかを使用して下さい。指定された言語以外での解答は評価されません。
+//---------------------------------------
 
 // Java
 package codeiq;
