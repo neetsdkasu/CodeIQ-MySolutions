075bcf2c8eb2ee2e358389a696950215d4e88589
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2013-10-12 15:13:23 +0900

    Update JavaHelloWorld.java

--- q431/JavaHelloWorld.java
+++ q431/JavaHelloWorld.java
@@ -1,32 +1,4 @@
 //---------------------------------------
-//CodeIQ  Q431
-// https://codeiq.jp/ace/cielavenir/q431
-// 
-// Quiz-Title: Restricted Words
-// Quiz-Author: @cielavenir cielavenir
-//              https://codeiq.jp/ace/cielavenir/
-//
-//問題文(抜粋)
-//
-// 標準出力に
-// Hello World
-// と出力するプログラムを作成して下さい。
-// 
-// ただし、数値、文字及び文字列リテラルを解答に含めることはできません。
-// Perlのqqやqw、Rubyの%Q、%q、%wなども避けたほうが評価が高くなります。
-// 言語仕様をフル活用して下さい！
-// 
-// プログラミング言語は
-// AppleScript(osascript)/C/C++/C#/Clojure/D/Erlang/Fortran/Go/Groovy/Haskell/
-// Hello Algorithm/HSP/Java/JavaScript(Node.js)/Kuin/Lisp/Lua/OCaml/Pascal/
-// Perl/PHP/Pike/Python/R/Ruby/Scala/Scheme/Smalltalk/VB.Net
-// のいずれかを使用して下さい。指定された言語以外での解答は評価されません。
-// (省略)
-//---------------------------------------
-
-/* 以下、私の解答コード */
-
-//---------------------------------------
 // My Answer for CodeIQ Q431
 // Author: Leonardone @ NEETSDKASU
 //---------------------------------------
